597c038562f8d6e780645d71373eb68b08fe644f
Lisa Schweinester <lisaschweinester@MacBook-Air.local>
2024-08-06 14:53:08 +0700

    Add Fraud.csv

---
+++ app/tests/test_simulation.py
@@ -0,0 +1,54 @@
+import unittest
+import pandas as pd
+from sqlalchemy import create_engine
+import mlflow
+from app import app
+import time
+
+# Mocking MLflow functions for testing
+mlflow.set_tracking_uri("http://localhost:5004")
+
+DATABASE_URL = 'sqlite:///fraud_detection.db'
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+class TestInsertData(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.client = app.test_client()
+
+    def test_insert_data(self):
+        file_path = 'simulated_data/simulated_data_year.csv'
+        all_data = pd.read_csv(file_path, delimiter=';')
+        self.insert_data(all_data)
+
+    def insert_data(self, df):
+        for week in range(1, 53):
+            weekly_data = df[df['week'] == week]
+            for _, row in weekly_data.iterrows():
+                input_data = {
+                    'step': row['step'],
+                    'type': row['type'],
+                    'amount': row['amount'],
+                    'nameOrig': row['nameOrig'],
+                    'oldbalanceOrg': row['oldbalanceOrg'],
+                    'newbalanceOrig': row['newbalanceOrig'],
+                    'nameDest': row['nameDest'],
+                    'oldbalanceDest': row['oldbalanceDest'],
+                    'newbalanceDest': row['newbalanceDest']
+                }
+
+                response = self.client.post('/dashboard', json=input_data)
+                self.assertEqual(response.status_code, 200)
+
+                # Call the prediction function
+                response = self.client.post('/predict', json=input_data)
+                self.assertEqual(response.status_code, 200)
+
+            print(f'Inserted data for week {week}')
+            time.sleep(2)  # Simulate time passing (adjust as needed)
+
+
+if __name__ == '__main__':
+    unittest.main()
