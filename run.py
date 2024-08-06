18e7a4be8246b952df26462ab3bc0197d5279e1a
Lisa Schweinester <lisaschweinester@ip-10-30-10-116.ap-southeast-1.compute.internal>
2024-08-06 13:02:21 +0700

    Add Fraud.csv

--- run.py
+++ run.py
@@ -2,10 +2,9 @@ from app import app
 import mlflow
 import os
 
-mlflow_tracking_uri = os.getenv("MLFLOW_TRACKING_URI", 'http://mlflow:5004')  # Updated port
-
-# Set MLflow tracking URI
+mlflow_tracking_uri = os.getenv("MLFLOW_TRACKING_URI", 'http://localhost:5004')  # Updated port
 mlflow.set_tracking_uri(mlflow_tracking_uri)
+mlflow.set_experiment('experiment')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5003)
